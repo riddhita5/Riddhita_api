a934784457f417c9f00f14fa0bed3eb05d92955f
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-07-12 20:17:33 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+
+define('HOST','localhost');
+define('USER','root');
+define('PASS','');
+define('DB','riddhita_test_api');
+
+    $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to connect');
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+echo "<h2>riddhita API</h2>";
+<ul>
+	
+	<!-- Connect -->
+	<li><a href='riddhita_user_info/connect.php'>Product Connect</a></li>
+
+	<!-- Signin & Signup-->
+    <li><a href='riddhita_user_info/signin.php'> Signin</a></li>
+	<li><a href='riddhita_user_info/signup.php'> Signup</a></li>
+	
+    <!-- User Info -->
+	<li><a href='riddhita_user_info/insert.php'>User Insert</a></li>
+	<li><a href='riddhita_user_info/view.php'>User View</a></li>
+	<li><a href='riddhita_user_info/update.php'>User Update</a></li>
+	<li><a href='riddhita_user_info/delete.php'>User Delete</a></li>
+
+    <!-- Product Data Info -->
+	<li><a href='riddhita_data_info/insert.php'>Product Insert</a></li>
+	<li><a href='riddhita_data_info/view.php'>Product View</a></li>
+	<li><a href='riddhita_data_info/update.php'>Product Update</a></li>
+	<li><a href='riddhita_data_info/delete.php'>Product Delete</a></li>
+    <li><a href='riddhita_data_info/upload.php'>Product Upload</a></li>
+
+</ul>
