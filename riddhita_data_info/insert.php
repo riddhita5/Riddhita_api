666f631ca5c194062aaff39307294505f0be10a6
Riddhita Gosai <riddhitagosai5@gmail.com>
2026-07-13 11:51:55 +0530

    Update insert.php

--- riddhita_data_info/insert.php
+++ riddhita_data_info/insert.php
@@ -5,17 +5,32 @@ $pname = $_POST["pname"];
 $pprice = $_POST["pprice"];
 $pdes = $_POST["pdes"];
 $pfeatures = $_POST["pfeatures"];
-$pimage = $_POST["pimage"];
 
+$upload_path = "images/";
+$upload_url = "http://" . $_SERVER['SERVER_NAME'] . "/riddhita_data_info/" . $upload_path;
 
-if($pname=="" && $pprice=="" && $pdes=="" && $pfeatures=="" && $pimage=="")
+$fileinfo = pathinfo($_FILES["pimage"]["name"]);
+$extension = $fileinfo["extension"];
+
+$random = "image_" . rand(1000,9999);
+
+$file_url = $upload_url . $random . "." . $extension;
+$file_path = $upload_path . $random . "." . $extension;
+
+move_uploaded_file($_FILES["pimage"]["tmp_name"], $file_path);
+
+$pimage = $file_url;
+
+if($pname=="" || $pprice=="" || $pdes=="" || $pfeatures=="")
 {
     echo "0";
 }
 else
 {
-    $sql = "INSERT INTO riddhita_data_info(pname,pprice,pdes,pfeatures,pimage)
-            VALUES('$pname','$pprice','$pdes','$pfeatures','$pimage')";
+    $sql = "INSERT INTO riddhita_data_info
+            (pname,pprice,pdes,pfeatures,pimage)
+            VALUES
+            ('$pname','$pprice','$pdes','$pfeatures','$pimage')";
 
     if(mysqli_query($con,$sql))
     {
@@ -26,4 +41,6 @@ else
         echo mysqli_error($con);
     }
 }
+
+mysqli_close($con);
 ?>
